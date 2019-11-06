d414c05f2a56294b960a427c0a414ba36e21308f
hcjuan04 <34015591+hcjuan04@users.noreply.github.com>
2019-11-06 10:37:18 -0500

    Update cheapWD-DB.sql

--- cheapWD-DB.sql
+++ cheapWD-DB.sql
@@ -1,4 +1,4 @@
 
-create table beacon (tstamp TIMESTAMP, channel INT, Enc CHAR (2), bssid CHAR (20), ssid CHAR (50), lat FLOAT, long FLOAT, alt FLOAT);
+create table beacon (tstamp TIMESTAMP, rssi INT, channel INT, Enc CHAR (2), bssid CHAR (20), ssid CHAR (50), lat FLOAT, long FLOAT, alt FLOAT);
 create table probe (tstamp TIMESTAMP,  probeAddr CHAR (20), ssid CHAR (50), lat FLOAT, long FLOAT, alt FLOAT);
 create table client (tstamp TIMESTAMP,  sta CHAR (20), bssid CHAR (20) );
